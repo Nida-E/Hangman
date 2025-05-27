67ff6b2bb3506116a6404dfb15590405613ffea8
Dark-StarStudios <146312613+Dark-StarStudios@users.noreply.github.com>
2025-05-27 09:59:12 +0200

    add auth to wordCreate

--- backend/routes/wordRoutes.js
+++ backend/routes/wordRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getWords, createWord } from '../controllers/wordController.js';
+import auth from '../config/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', getWords);
-router.post('/create', createWord);
+router.post('/create', auth, createWord);
 
 export default router;
